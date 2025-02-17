1ff8a4784609b76b13acc50300037f8ffd3e0e97
Venchhut <ijinbaekk@gmail.com>
2025-02-17 23:53:42 +0700

    fix: remove props-type

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // Vite config
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["prop-types"],
-    },
-  },
   plugins: [react()],
   assetsInclude: ["**/*.jpg", "**/*.jpeg", "**/*.png"],
 });
